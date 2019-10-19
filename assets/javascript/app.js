213b27abf2a803c8082c8c1ba175c834264439dc
CaoKao <bksaechao@gmail.com>
2019-10-19 15:11:12 -0700

    adjusted the if statements to properly add a point for each correct answer and inserted additional user feedback responses based on the users score.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,40 +3,47 @@ window.onload = function () {
     $("#gameEnd").on("click", timeend);
 }
 
-//Static Variables
+//Set variables
 var time = 60;
 var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+var corrArr = ["Theodor Seuss Geisel", "2007", "Carbon dioxide", "Marky Mark", "Bugs Bunny", "Apple", "Dr. Evil", "Tim Burton"]
+
 $("#time-left").text(60);
 
-//Countdown functions
+//Countdown function
 function countdown() {
     time--;
     $("#time-left").html(time);
     if (time === 0) {
-        clearInterval(intervalId);
+        radioResults();
+        $("#corr").text(correct);
+        $("#incorr").text(incorrect);
+        $("#unans").text(unanswered);
         $("#trivStart").hide();
         $("#trivEnd").show();
-        $("#alert").text("Times Up!");
     }
 }
-
+//Calls on the countdown function, hide intro page, 
+//and displays trivia page once begin is clicked.
 function timestart() {
     $("#trivIntro").hide();
     $("#trivStart").show();
     intervalId = setInterval(countdown, 1000);
 }
 
-//Function that is executed when Done is clicked
+//Runs the results function, hides the trivia page, and displays the results page
 function timeend() {
 
     //get the test results
     radioResults();
+    $("#corr").text(correct);
+    $("#incorr").text(incorrect);
+    $("#unans").text(unanswered);
 
     $("#trivStart").hide();
     $("#trivEnd").show();
-    $("#alert").text("Excellent!");
-
-
 }
 
 
@@ -58,9 +65,39 @@ function radioResults() {
     console.log(question_7);
     var question_8 = $("input[type='radio'][name='movie']:checked").val();
     console.log(question_8);
-}
 
-// $("input[type='radio']").click(function () {
-//     var question_1 = $("input[name='name']:checked").val();
-//     console.log(radioValue)
-// });
+    //stoping the time.
+    clearInterval(intervalId);
+
+    //array to store all the user's answers.
+    var questionArray = [];
+
+    questionArray.push(question_1);
+    questionArray.push(question_2);
+    questionArray.push(question_3);
+    questionArray.push(question_4);
+    questionArray.push(question_5);
+    questionArray.push(question_6);
+    questionArray.push(question_7);
+    questionArray.push(question_8);
+
+    for (var i = 0; i < questionArray.length; i++) {
+        if (questionArray[i] === corrArr[i]) {
+            correct++;
+        } else if (questionArray[i] === undefined) {
+            unanswered++;
+        } else {
+            incorrect++;
+        }
+    }
+
+    if (correct === 8) {
+        $("#alert").text("Perfect!")
+    } else if (correct < 8 && correct >= 6) {
+        $("#alert").text("Good Job!")
+    } else if (correct <= 5 && correct > 0) {
+        $("#alert").text("Nice Try!")
+    } else {
+        $("#alert").text("=(")
+    }
+}
