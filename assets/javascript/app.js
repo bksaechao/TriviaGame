8f84889b0d9f5ed49614421b0cb95b4da63d8026
CaoKao <bksaechao@gmail.com>
2019-10-19 07:06:24 -0700

    set conditions to end game and display result page once time ran out

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,34 @@
 window.onload = function () {
     $("#gameStart").on("click", timestart);
+    $("#gameEnd").on("click", timeend);
 }
-time = 120;
+
+//Static Variables
+time = 10;
 $("#time-left").text(120);
 
+//Countdown functions
 function countdown() {
     time--;
     $("#time-left").html(time);
     console.log(time);
+    if (time === 0) {
+        clearInterval(intervalId);
+        $("#trivStart").hide();
+        $("#trivEnd").show();
+        $("#alert").text("Times Up!");
+    }
 }
 
 function timestart() {
     $("#trivIntro").hide();
     $("#trivStart").show();
     intervalId = setInterval(countdown, 1000);
+}
+
+//Function that is executed when Done is clicked
+function timeend() {
+    $("#trivStart").hide();
+    $("#trivEnd").show();
+    $("#alert").text("Excellent!");
 }
